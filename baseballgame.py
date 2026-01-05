f85b6561933286fed549f9fdb46eef1cf8f7232e
naroyounho-hub <naroyounho@gmail.com>
2026-01-05 21:40:16 +0900

    gpt에게 2번 물어봄
    으아아아아

--- baseballgame.py
+++ baseballgame.py
@@ -3,13 +3,12 @@
 import random
 
 a = random.sample(range(1, 10), 3)
-a2 = f"{str(a[0])+str(a[1])+str(a[2])}"
+a2 = f"{a[0]}{a[1]}{a[2]}"
 print(a2)
-a1 = int(input("숫자 : "))
 while True:
+    a1 = int(input("숫자 : "))
     s = 0
     b = 0
-    print(a1)
     if a1 // 100 == int(a2) // 100:
         s += 1
     elif a1 // 100 in a:
@@ -22,6 +21,7 @@ while True:
         s += 1
     elif a1 % 10 in a:
         b += 1
-        print(f"(스트라이크 : {s} / 볼 : {b})")
+    print(f"(스트라이크 : {s} / 볼 : {b})")
     if s == 3:
         print("정답")
+        break
