be42da47ed8feb03e6339fb37f3d2451556ee889
naroyounho-hub <naroyounho@gmail.com>
2026-01-05 21:34:17 +0900

    오류를 못고침

--- baseballgame.py
+++ baseballgame.py
@@ -3,22 +3,25 @@
 import random
 
 a = random.sample(range(1, 10), 3)
-a2 = f"{str(a[0])+str(a[1])+str(a[2])})"
+a2 = f"{str(a[0])+str(a[1])+str(a[2])}"
 print(a2)
-b = 0
-a1 = print(input("숫자 : "))
-s = 0
-while s == 3:
-    if a1 // 100 == a2 // 100:
+a1 = int(input("숫자 : "))
+while True:
+    s = 0
+    b = 0
+    print(a1)
+    if a1 // 100 == int(a2) // 100:
         s += 1
     elif a1 // 100 in a:
         b += 1
-        if (a1 % 100) // 10 == (a2 % 100) // 10:
-            s += 1
-        elif (a1 % 100) // 10 in a:
-            b += 1
-            if a1 % 10 == a2 % 10:
-                s += 1
-            elif a1 % 10 in a:
-                b += 1
-                print(f"(스트라이크 : {s} / 볼 : {b})")
+    if (a1 % 100) // 10 == (int(a2) % 100) // 10:
+        s += 1
+    elif (a1 % 100) // 10 in a:
+        b += 1
+    if a1 % 10 == int(a2) % 10:
+        s += 1
+    elif a1 % 10 in a:
+        b += 1
+        print(f"(스트라이크 : {s} / 볼 : {b})")
+    if s == 3:
+        print("정답")
